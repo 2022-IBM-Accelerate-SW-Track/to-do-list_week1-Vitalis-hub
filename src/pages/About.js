ac84da65272bb7e5f32178f57a66f50e0e5d3ca1
Vitalis-hub <Michealvitalis123@gmail.com>
2022-06-09 18:14:06 -0500

    Pushing UI component changes to the repo

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import temp from "./../assets/temp.jpg";
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+        <div class="split left">
+          <div className="centered">
+            <img src = {temp}
+              className="profile_image"
+              // Image goes here
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Vitalis Ibe</div>
+            <div className="brief_description">
+              My name is Vitalis Ibe. I am curently pusuring my degree in computer science at the University of Central Oklahoma<br/>Some of my hobbies include movies, games, and sport
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     )
   }
